e8151588ff2b56645fc569d6681a1bc952aff36e
egee-irl <brian@egee.io>
2020-07-15 15:04:07 -0700

    retitled comments

--- http-request/request.py
+++ http-request/request.py
@@ -1,19 +1,4 @@
-## Egee Style
-import requests
-import json
-import os
-
-if __name__ == "__main__":
-  orphans = []
-  for key, value in json.loads(requests.get('https://api.vultr.com/v1/server/list', headers = {'API-Key': os.environ['VULTR_API_KEY']}).text).items():
-    if value['tag'] == 'slurmci':
-      orphans.append(value)
-
-  for server in orphans:
-    print(requests.post('https://api.vultr.com/v1/server/destroy', headers = {'API-Key': os.environ['VULTR_API_KEY']}, data = {'SUBID':server["SUBID"]}).status_code)
-
-
-## Pythonic Style
+## Pythonic
 import requests
 import json
 import os
@@ -36,3 +21,17 @@ if __name__ == "__main__":
 
     destroy_request = requests.post(destroy_url, headers=headers, data=data)
     print(destroy_request.status_code)
+
+## Minified
+import requests, json, os
+
+if __name__ == "__main__":
+  orphans = []
+  for key, value in json.loads(requests.get('https://api.vultr.com/v1/server/list', headers = {'API-Key': os.environ['VULTR_API_KEY']}).text).items():
+    if value['tag'] == 'slurmci':
+      orphans.append(value)
+
+  for server in orphans:
+    print(requests.post('https://api.vultr.com/v1/server/destroy', headers = {'API-Key': os.environ['VULTR_API_KEY']}, data = {'SUBID':server["SUBID"]}).status_code)
+
+
